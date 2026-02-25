91f680c0498bfa7f28abc9d48b35f51f53bf7900
christianinglis <ccinglis@my.waketech.edu>
2026-02-25 11:57:45 -0500

    const

---
+++ DOMTimer.js
@@ -0,0 +1,7 @@
+let timer = 0;
+let intervalID = null;
+
+const display = document.getElementById("timerDisplay");
+const startBtn = document.getElementById("startTimer");
+const stopBtn = document.getElementById("stoptTimer");
+const resetBtn = document.getElementById("resetTimer");
