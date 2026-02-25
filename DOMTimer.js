8b6bd3a2c7fa2847afc8f4738567e4b7b222b8c1
christianinglis <ccinglis@my.waketech.edu>
2026-02-25 12:00:06 -0500

    startBtn

--- DOMTimer.js
+++ DOMTimer.js
@@ -5,3 +5,13 @@ const display = document.getElementById("timerDisplay");
 const startBtn = document.getElementById("startTimer");
 const stopBtn = document.getElementById("stoptTimer");
 const resetBtn = document.getElementById("resetTimer");
+
+startBtn.addEventListener("click", function(){
+
+    if(intervalID == null) {
+        intervalID == setInterval(function() {
+            timer++;
+            display.innerHTML = timer;
+        }, 1000);
+    }
+});
